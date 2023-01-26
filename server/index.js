4d768438a4e8791e5ffae74984904c560e40fc52
Dorjee <rishav345@gmail.com>
2023-01-25 22:38:53 -0500

    adding requests in the server

--- server/index.js
+++ server/index.js
@@ -15,34 +15,28 @@ app.get("/api/chats", (req, res) => {
       { text: "I know so cool right!" },
       { text: "This is a lot of fun!" },
     ],
-  },);
+  });
 });
 
-// creating chat mesaages 
+// creating chat mesaages
 app.post("/api/messages", (req, res) => {
-   res.send("Successfully connected with the server")
-})
+  res.send("Successfully connected with the server");
+});
 
-
-// get all the users 
+// get all the users
 app.get("/api/users", (req, res) => {
-   console.log(' All users will be retrevied from this requests!')
+  console.log(" All users will be retrevied from this requests!");
 });
 
-
-//create user 
+//create user
 app.post("/api/users", (req, res) => {
-  res.send("creating all the users...")
-})
-
-
-// getting the users by id 
-app.get("api/users/{id}", (req, res) => {
-  console.log(' All users will be retrevied from this requests!')
+  res.send("creating all the users...");
 });
 
-
-
+// getting the users by id
+app.get("api/users/{id}", (req, res) => {
+  console.log(" All users will be retrevied from this requests!");
+});
 
 app.listen(3001, () => {
   console.log(`Example app listening on port ${port}`);
