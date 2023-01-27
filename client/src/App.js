f0510e7c3f5f4dd71c68c78f4c3155a5be612d41
Dorjee <rishav345@gmail.com>
2023-01-26 23:40:29 -0500

    additional requests

--- client/src/App.js
+++ client/src/App.js
@@ -34,19 +34,25 @@ function App() {
 
 
   // create a user post request  api/users
-
-  
+  const createUsers = () => {
+    axios.post('/api/users', {
+       messages: 'Hello there friend'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
 
   // get single  user  api/users/{id}
-
-
-
+  useEffect(() => {
+    axios.get("/api/users/{id}").then((response) => setUsers(response.data));
+    console.log(users);
+  }, []);
 
   console.log(message);
-
-  
-
-
 
   return (
     <div className="App">
--- server/index.js
+++ server/index.js
@@ -20,17 +20,30 @@ app.get("/api/chats", (req, res) => {
 
 // creating chat mesaages
 app.post("/api/messages", (req, res) => {
-  res.send("Successfully connected with the server");
+  //res.send("Successfully connected with the server");
+  const data = req.body;
+  // Do something with the data
+  res.json({ messages: [
+    { text: "Hey there!" },
+    { text: "How is everybody doing ?" },
+    { text: "So glad this chat is up and running" },
+  ], currentmessages: [
+    { text: "I know so cool right!" },
+    { text: "This is a lot of fun!" },
+  ],});
 });
 
 // get all the users
 app.get("/api/users", (req, res) => {
-  console.log(" All users will be retrevied from this requests!");
+   res.send({name: "RishavA", currentuser:"You"})
 });
 
 //create user
 app.post("/api/users", (req, res) => {
-  res.send("creating all the users...");
+  const data = req.body;
+  console.log(data);
+  // Do something with the data
+  res.json({ message: "Data received" });
 });
 
 // getting the users by id
